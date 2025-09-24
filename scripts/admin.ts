4a19d0f0822120021efb4154beefd3edb1ef2bd3
Aljabar <aljabarprl@gmail.com>
2025-09-24 09:33:00 +0700

    Update admin.ts

--- scripts/admin.ts
+++ scripts/admin.ts
@@ -35,8 +35,8 @@ system.afterEvents.scriptEventReceive.subscribe((event: ScriptEventCommandMessag
     //show:variables
     if (event.id === "show:variables") {
         
-        world.sendMessage(`§aDebug Message:§r ${DebugMessage}`);
-        world.sendMessage(`§aMax Players:§r ${maxPlayers}`);
+        world.sendMessage(`§aDebug Message:§r ${DebugMessage.toString()}`);
+        world.sendMessage(`§aMax Players:§r ${maxPlayers.toString()}`);
     }
 });
 
